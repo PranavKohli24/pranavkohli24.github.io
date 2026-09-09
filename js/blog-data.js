ad1d2ca098bb6e8d01ef7b0c003861297dff82d7
PranavKohli24 <kohlipranav24@gmail.com>
2026-09-10 03:13:22 +0530

    routing bug fixed

--- js/blog-data.js
+++ js/blog-data.js
@@ -8,9 +8,9 @@ const blogPosts = [
         id: 'blog1',
         title: 'How ChatGPT helped me answer much of my curiousity',
         date: 'November 2025',
-        image: 'src/images/blog1.jpeg',
+        image: '/src/images/blog1.jpeg',
         imageAlt: 'Blog1 Image',
-        audioFile: 'src/audio/blog1_audio.mp3',
+        audioFile: '/src/audio/blog1_audio.mp3',
         content: `
             <p>I've always been like this.</p>
 
@@ -69,9 +69,9 @@ const blogPosts = [
     id: 'blog2',
     title: 'The 3 AM Breakthrough: Saving the Hackathon Demo',
     date: 'December 2025',
-    image: 'src/images/blog2.jpeg',
+    image: '/src/images/blog2.jpeg',
     imageAlt: 'Hackathon team working late night',
-    audioFile: 'src/audio/blog2_audio.mp3',
+    audioFile: '/src/audio/blog2_audio.mp3',
     content: `
             <p>It was a 24 hour hackathon</p>
         <p>It was 3:47 AM. The submission deadline was at 8:00 AM sharp.</p>
